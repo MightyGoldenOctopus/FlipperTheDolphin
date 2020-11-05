04f2ab7320d0b62462636d80722b750f5aae3eb0
Jean-Adrien DUCASTAING <jean-adrien.ducastaing@epita.fr>
2020-11-05 10:22:50 +0100

    pso: complete functional pso class

--- src/pso.py
+++ src/pso.py
@@ -2,11 +2,11 @@ import numpy as np
 
 
 class Particle:
-    def __init__(self, fitness, x, v, misc=None):
+    def __init__(self, id, fitness, x, v):
+        self.id = id
         self.fitness = fitness
         self.x = x
         self.v = v
-        self.misc = misc
         self.best_p = {
             "position:": x,
             "fitness": fitness
@@ -23,6 +23,7 @@ class ParticleSwarmOptimizer:
         self.pop_size = params["pop_size"]
         self.population = {}
         self.w = params["hyperparameters"]["initertia"]
+        self.w_damp = params["hyperparameters"]["inertia_dampening"]
         self.c1 = params["hyperparameters"]["cognitive_acceleration"]
         self.c2 = params["hyperparameters"]["social_acceleration"]
         # Problem constraints
@@ -33,38 +34,51 @@ class ParticleSwarmOptimizer:
         self.fitness_function = fitness_function
         # Global best position
         self.best_g = {
+            "id": None,
             "position": None,
             "fitness": None
         }
         self.best_g_history = []
 
-    def initialize(self, particles):
-        if len(particles.keys()) > self.pop_size:
-            raise AttributeError("Population size and particles dict size don't match")
-        for id in particles.keys():
-            position, velocity = particles[id]["position"], particles[id]["velocity"]
-            if not position:
-                position = np.random.uniform(low=self.var_min,
+    def initialize(self):
+        for i in range(self.pop_size):
+            position = np.random.uniform(low=self.var_min,
                                              high=self.var_max,
                                              size=(self.n_var,))
-            if not velocity:
-                velocity = np.zeros(shape=(self.n_var,))
-            misc = particles[id]["misc"]
-            self.population[id] = Particle(None, position, velocity, misc)
-            self.evaluate_fitness(self.population[id])
+            velocity = np.zeros(shape=(self.n_var,))
+            self.population[i] = Particle(None, i, position, velocity)
+            self.evaluate_fitness(self.population[i])
 
     def evaluate_fitness(self, particle):
         particle.fitness = self.fitness_function(particle.x)
-        if not particle.best_p["fitness"] or particle.fitness > particle.best_p["fitness"]:
+        if not particle.best_p["fitness"] or particle.best_p["fitness"] > particle.best_p["fitness"]:
             particle.best_p = {
                 "position": particle.x,
                 "fitness": particle.fitness
             }
-        if not self.best_g["fitness"] or particle.fitness > self.best_g["fitness"]:
+        if not self.best_g["fitness"] or particle.best_p["fitness"] > self.best_g["fitness"]:
             self.best_g = {
+                "id": particle.id,
                 "position": particle.x,
                 "fitness": particle.fitness
             }
 
     def run(self, verbose=False):
-        # Do stuff...
+        for i in range(self.max_it):
+            for id, particle in self.population.items():
+                valid_move, new_position, timeout = False, None, 10
+                while not valid_move and timeout > 0:
+                    timeout -= 1
+                    new_velocity = self.w * particle.v \
+                        + self.c1 * np.random.uniform(size=(self.n_var,)) @ particle.distance(particle.best_p["position"]) \
+                        + self.c2 * np.random.uniform(size=(self.n_var,)) @ particle.distance(self.best_g["position"])
+                    new_position = particle.x + new_velocity
+                    valid_move = (new_position >= self.var_min) and (new_position <= self.var_max)
+                if new_position:
+                    particle.x = new_position
+                self.evaluate_fitness(particle)
+            self.best_g_history.append(self.best_g)
+            self.w *= self.w_damp
+            if verbose:
+                print(f"Iteration {i}/{self.max_it}; Best particle id: {self.best_g['id']}; Best fitness: {self.best_g['fitness']}")
+            return self.best_g, self.best_g_history
