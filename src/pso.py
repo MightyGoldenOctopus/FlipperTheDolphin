f888c08b6a0e52f191bbf60eac0c840087f18925
Audran Doublet <audran.doublet@epita.fr>
2020-11-07 12:37:31 +0100

    pso: add progress bar & small fixes

--- src/pso.py
+++ src/pso.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from tqdm import trange
 
 class Particle:
     def __init__(self, id, fitness, x, v):
@@ -12,26 +12,31 @@ class Particle:
             "fitness": fitness
         }
 
+
     def distance(self, target):
         return np.subtract(target, self.x)
 
 
 class ParticleSwarmOptimizer:
     def __init__(self, params, constraints, fitness_function):
+        self.population = {}
+
         # PSO parameters
         self.max_it = params["max_it"]
         self.pop_size = params["pop_size"]
-        self.population = {}
-        self.w = params["hyperparameters"]["initertia"]
+        self.w = params["hyperparameters"]["inertia"]
         self.w_damp = params["hyperparameters"]["inertia_dampening"]
         self.c1 = params["hyperparameters"]["cognitive_acceleration"]
         self.c2 = params["hyperparameters"]["social_acceleration"]
+
         # Problem constraints
         self.n_var = constraints["n_var"]
         self.var_min = constraints["var_min"]
         self.var_max = constraints["var_max"]
+
         # Fitness function
         self.fitness_function = fitness_function
+
         # Global best position
         self.best_g = {
             "id": None,
@@ -39,6 +44,7 @@ class ParticleSwarmOptimizer:
             "fitness": None
         }
         self.best_g_history = []
+
 
     def initialize(self):
         for i in range(self.pop_size):
@@ -49,8 +55,10 @@ class ParticleSwarmOptimizer:
             self.population[i] = Particle(None, i, position, velocity)
             self.evaluate_fitness(self.population[i])
 
+
     def evaluate_fitness(self, particle):
         particle.fitness = self.fitness_function(particle.x)
+
         if not particle.best_p["fitness"] or particle.best_p["fitness"] > particle.best_p["fitness"]:
             particle.best_p = {
                 "position": particle.x,
@@ -63,15 +71,18 @@ class ParticleSwarmOptimizer:
                 "fitness": particle.fitness
             }
 
+
     def run(self, verbose=False):
-        for i in range(self.max_it):
+        t = trange(self.max_it, desc='PSO Run', leave=True)
+
+        for i in t:
             for id, particle in self.population.items():
                 valid_move, new_position, timeout = False, None, 10
                 while not valid_move and timeout > 0:
                     timeout -= 1
                     new_velocity = self.w * particle.v \
-                        + self.c1 * np.random.uniform(size=(self.n_var,)) @ particle.distance(particle.best_p["position"]) \
-                        + self.c2 * np.random.uniform(size=(self.n_var,)) @ particle.distance(self.best_g["position"])
+                        + self.c1 * np.random.uniform(size=(self.n_var,)) * particle.distance(particle.best_p["position"]) \
+                        + self.c2 * np.random.uniform(size=(self.n_var,)) * particle.distance(self.best_g["position"])
                     new_position = particle.x + new_velocity
                     valid_move = (new_position >= self.var_min) and (new_position <= self.var_max)
                 if new_position:
@@ -79,6 +90,8 @@ class ParticleSwarmOptimizer:
                     self.evaluate_fitness(particle)
             self.best_g_history.append(self.best_g)
             self.w *= self.w_damp
-            if verbose:
-                print(f"Iteration {i}/{self.max_it}; Best particle id: {self.best_g['id']}; Best fitness: {self.best_g['fitness']}")
+
+            t.set_description(f"PSO run (best particle id: {self.best_g['id']}, best fitness: {self.best_g['fitness']})")
+            t.refresh() # to show immediately the update
+
         return self.best_g, self.best_g_history
